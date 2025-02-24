3c53a1bf8e8c207656eab82d66c3459183a00f22
Rahul Nayanegali <rahulnayanegali@gmail.com>
2025-02-24 18:40:58 -0500

    removed login

--- Project/my-react-app/src/App.js
+++ Project/my-react-app/src/App.js
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Main from './components/main/main.js';
-import Landing from './pages/Landing.js';
 import { AuthProvider } from './components/session/AuthContext.js';
 import { auth } from './firebase.js'; // Import auth from firebase.js
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import PatientLogin from './pages/PatientLogin.js';
-import PatientSignUp from './pages/psignup.js';
-import DoctorLogin from './pages/DoctorLogin.js';
-import DoctorSignUp from './pages/dsignup.js';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
     const [user, setUser] = useState(null);
 
@@ -25,19 +20,7 @@ function App() {
         <BrowserRouter>
             <AuthProvider>
                 <div className="App overflow-y-scroll no-scrollbar">
-
-                    <Routes>
-                        <Route path='*' element={
-                            user
-                                ? <Main user={user} />
-                                : <Landing />
-                            //: <DoctorSignup/>
-                        } />
-                        <Route path='/patientlogin' element={<PatientLogin />} />
-                        <Route path='/patientsignup' element={<PatientSignUp />} />
-                        <Route path='/doctorlogin' element={<DoctorLogin />} />
-                        <Route path='/doctorsignup' element={<DoctorSignUp />} />
-                    </Routes>
+                <Main user={user} />
 
                 </div>
             </AuthProvider>
--- Project/my-react-app/src/components/main/main.js
+++ Project/my-react-app/src/components/main/main.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../navbar/navbar';
 import Submain from '../submain/submain';
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from 'react-router-dom';
+import { db } from '../../firebase';
+import { useAuth } from '../../components/session/AuthContext'; // Import useAuth hook
+
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
 import Test from '../../test/usertype'
 const Main = () => {
-    return (
-        <div className="h-screen w-screen bg-secondary justify-start items-center inline-flex">
-        <Navbar/>
-        <Submain/>
-        
-         {/* <Test/> */}
+
+  const { setUser, setUserType, user } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+
+
+  const handleLogin = async (email, password, type) => {
+    try {
+      // Authenticate user
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const currentUser = userCredential.user;
+
+      if (currentUser) {
+        // Check user type
+        const userTypeDocRef = doc(db, 'Medical Professional', currentUser.uid);
+        const userTypeDocSnapshot = await getDoc(userTypeDocRef);
+
+        if (userTypeDocSnapshot.exists()) {
+          // Authorized user
+          setUser(currentUser);
+          setUserType(type);
+          localStorage.setItem('user', JSON.stringify(currentUser));
+          localStorage.setItem('userType', type);
+          navigate('/');
+        } else {
+          // Unauthorized user
+          setError("You are not authorized to log in as a docotor.");
+          await signOut(auth);
+        }
+      } else {
+        console.log("No user signed in");
+        // Handle case where no user is signed in
+      }
+    } catch (error) {
+      // Handle errors
+      setError(error.message); // Display error message to user
+    }
+  };
+
+  useEffect(() => {
+    handleLogin('patricia.normann@presbytarian.org', 3451231, "doctor")
+  }, [])
+
+  if (!user) {
+    return <div className='h-full w-full text-white inline-flex justify-center items-center'>Loading...</div>; // Render loading indicator
+  }
+  return (
+    <div className="h-screen w-screen bg-secondary justify-start items-center inline-flex">
+      <Navbar />
+      <Submain />
+
+      {/* <Test/> */}
     </div>
-    );
+  );
 };
 
 export default Main;
